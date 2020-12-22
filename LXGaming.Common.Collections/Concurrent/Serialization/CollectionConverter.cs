12b38f670ef9a28e9c94ec6381a2e0b80e11a324
Alex Thomson <LXGaming@users.noreply.github.com>
2020-12-23 03:40:25 +1300

    Fix incompatibility with System.Text.Json
    During Serialization System.Text.Json does not appear to be disposing the IEnumerator resulting in the Collection being locked indefinitely.
    
    This commit adds a workaround for this issue.

---
+++ LXGaming.Common.Collections/Concurrent/Serialization/CollectionConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LXGaming.Common.Collections.Concurrent.Serialization {
+
+    public class CollectionConverter<TCollection, TItem> : JsonConverter<TCollection> where TCollection : ICollection<TItem> {
+
+        public override TCollection Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+            if (reader.TokenType != JsonTokenType.StartArray) {
+                throw new JsonException($"Unexpected TokenType (got {nameof(reader.TokenType)}, expected {nameof(JsonTokenType.StartArray)})");
+            }
+
+            reader.Read();
+
+            var items = Activator.CreateInstance<TCollection>();
+            if (items == null) {
+                throw new JsonException($"Failed to create an instance of {nameof(TCollection)}");
+            }
+
+            while (reader.TokenType != JsonTokenType.EndArray) {
+                var item = JsonSerializer.Deserialize<TItem>(ref reader, options);
+                if (item == null) {
+                    throw new JsonException($"Failed to deserialize {nameof(TItem)}");
+                }
+
+                items.Add(item);
+                reader.Read();
+            }
+
+            return items;
+        }
+
+        public override void Write(Utf8JsonWriter writer, TCollection value, JsonSerializerOptions options) {
+            writer.WriteStartArray();
+
+            foreach (var item in value) {
+                JsonSerializer.Serialize(writer, item, options);
+            }
+
+            writer.WriteEndArray();
+        }
+    }
+}
---
+++ LXGaming.Common.Collections/Concurrent/Serialization/ConcurrentCollectionFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LXGaming.Common.Collections.Concurrent.Serialization {
+
+    public class ConcurrentCollectionFactory : JsonConverterFactory {
+
+        public override bool CanConvert(Type typeToConvert) {
+            return typeToConvert.IsGenericType && GetBaseTypes(typeToConvert)
+                .Where(type => type.IsGenericType)
+                .Any(type => type.GetGenericTypeDefinition() == typeof(ConcurrentCollection<,>));
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options) {
+            var itemType = typeToConvert.GetGenericArguments()[0];
+            var converter = (JsonConverter) Activator.CreateInstance(
+                typeof(CollectionConverter<,>).MakeGenericType(typeToConvert, itemType),
+                BindingFlags.Instance | BindingFlags.Public,
+                null, null, null)!;
+
+            return converter;
+        }
+
+        private IEnumerable<Type> GetBaseTypes(Type type) {
+            var baseType = type.BaseType;
+            while (baseType != null && baseType != typeof(object)) {
+                yield return baseType;
+                baseType = baseType.BaseType;
+            }
+        }
+    }
+}
