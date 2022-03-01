7a7242aaa6c915b4c297f697e31af33e73b1f372
Alex Thomson <LXGaming@users.noreply.github.com>
2022-03-01 15:05:26 +1300

    Create Hosting

---
+++ LXGaming.Common.Hosting/Extensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace LXGaming.Common.Hosting {
+
+    public static class Extensions {
+
+        public static IServiceCollection AddAllServices(this IServiceCollection services, Assembly assembly) {
+            foreach (var type in assembly.GetTypes()) {
+                services.AddService(type);
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddService<TService>(this IServiceCollection services) where TService : class {
+            return services.AddService(typeof(TService));
+        }
+
+        public static IServiceCollection AddService(this IServiceCollection services, Type type) {
+            var serviceAttribute = type.GetCustomAttribute<ServiceAttribute>();
+            if (typeof(IHostedService).IsAssignableFrom(type)) {
+                if (serviceAttribute == null) {
+                    return services.AddSingleton(typeof(IHostedService), type);
+                }
+
+                if (serviceAttribute.Lifetime == ServiceLifetime.Singleton) {
+                    return services.AddHostedService(serviceAttribute.Type ?? type, type);
+                }
+
+                throw new InvalidOperationException($"{nameof(IHostedService)} cannot be {serviceAttribute.Lifetime}");
+            }
+
+            if (serviceAttribute != null) {
+                services.Add(new ServiceDescriptor(serviceAttribute.Type ?? type, type, serviceAttribute.Lifetime));
+            }
+
+            return services;
+        }
+
+        private static IServiceCollection AddHostedService(this IServiceCollection services, Type serviceType, Type implementationType) {
+            return services
+                .AddSingleton(serviceType, implementationType)
+                .AddSingleton(provider => (IHostedService) provider.GetRequiredService(serviceType));
+        }
+    }
+}
---
+++ LXGaming.Common.Hosting/ServiceAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LXGaming.Common.Hosting {
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ServiceAttribute : Attribute {
+
+        public ServiceLifetime Lifetime { get; }
+        public Type? Type { get; }
+
+        public ServiceAttribute(ServiceLifetime lifetime, Type? type = null) {
+            Lifetime = lifetime;
+            Type = type;
+        }
+    }
+}
