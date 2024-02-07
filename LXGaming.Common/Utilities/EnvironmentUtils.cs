69ba5bdef92a2da60b274e6e0c6a548ba8bb1361
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2024-02-07 22:59:12 +1300

    Add Environment methods

--- LXGaming.Common/Utilities/EnvironmentUtils.cs
+++ LXGaming.Common/Utilities/EnvironmentUtils.cs
@@ -4,8 +4,34 @@ namespace LXGaming.Common.Utilities;
 
 public static class EnvironmentUtils {
 
+    public const string Development = "Development";
+    public const string Staging = "Staging";
+    public const string Production = "Production";
+
+    public static bool IsDevelopment() {
+        return IsEnvironment(Development);
+    }
+
+    public static bool IsStaging() {
+        return IsEnvironment(Staging);
+    }
+
+    public static bool IsProduction() {
+        return IsEnvironment(Production);
+    }
+
+    public static bool IsEnvironment(string environment) {
+        return string.Equals(GetEnvironment(), environment, StringComparison.OrdinalIgnoreCase);
+    }
+
     public static bool IsRunningInContainer() {
         return string.Equals(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"), "true", StringComparison.OrdinalIgnoreCase);
+    }
+
+    public static string GetEnvironment() {
+        return Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+               ?? Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")
+               ?? Production;
     }
 
     public static DateTime GetStartTime() {
