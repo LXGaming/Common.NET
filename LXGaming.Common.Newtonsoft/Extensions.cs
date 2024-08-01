f23b3c3b66494c9c5659ffa9a141172b6469f462
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2024-08-01 20:43:15 +1200

    Add Newtonsoft Extensions

---
+++ LXGaming.Common.Newtonsoft/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LXGaming.Common.Newtonsoft;
+
+public static class Extensions {
+
+    public static T ToRequiredObject<T>(this JToken token) {
+        return token.ToObject<T>() ?? throw new JsonException($"Failed to deserialize {nameof(T)}");
+    }
+}
