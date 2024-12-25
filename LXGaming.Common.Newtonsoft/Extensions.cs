584e2230316bfbc580f2479ee93531e86ed49dda
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2024-12-25 22:57:07 +1300

    Fix getting generic type name

--- LXGaming.Common.Newtonsoft/Extensions.cs
+++ LXGaming.Common.Newtonsoft/Extensions.cs
@@ -6,6 +6,6 @@ namespace LXGaming.Common.Newtonsoft;
 public static class Extensions {
 
     public static T ToRequiredObject<T>(this JToken token) {
-        return token.ToObject<T>() ?? throw new JsonException($"Failed to deserialize {nameof(T)}");
+        return token.ToObject<T>() ?? throw new JsonException($"Failed to deserialize {typeof(T).Name}");
     }
 }
