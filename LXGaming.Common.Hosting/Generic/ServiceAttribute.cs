3a0a88f230208a6cdff33c68016ba177ff66622e
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2024-12-31 22:42:33 +1300

    Remove Common.Hosting
    Use https://github.com/LXGaming/Hosting.NET

--- LXGaming.Common.Hosting/Generic/ServiceAttribute.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.Extensions.DependencyInjection;
-
-namespace LXGaming.Common.Hosting.Generic;
-
-[AttributeUsage(AttributeTargets.Class, Inherited = false)]
-public class ServiceAttribute<T>(ServiceLifetime lifetime) : ServiceAttribute(lifetime, typeof(T));
