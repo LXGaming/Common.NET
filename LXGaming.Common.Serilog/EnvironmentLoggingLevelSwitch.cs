2d1ba51925eaa5b88c2c30350b905014a7f96c49
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2025-07-23 14:59:30 +1200

    Support Staging environment

--- LXGaming.Common.Serilog/EnvironmentLoggingLevelSwitch.cs
+++ LXGaming.Common.Serilog/EnvironmentLoggingLevelSwitch.cs
@@ -5,8 +5,20 @@ namespace LXGaming.Common.Serilog;
 
 public class EnvironmentLoggingLevelSwitch : LoggingLevelSwitch {
 
-    public EnvironmentLoggingLevelSwitch(LogEventLevel developmentLevel = LogEventLevel.Debug, LogEventLevel productionLevel = LogEventLevel.Information) {
-        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
-        MinimumLevel = string.Equals(environment, "Development", StringComparison.OrdinalIgnoreCase) ? developmentLevel : productionLevel;
+    public EnvironmentLoggingLevelSwitch(LogEventLevel developmentLevel, LogEventLevel stagingLevel,
+        LogEventLevel productionLevel) : base(productionLevel) {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+                          ?? Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+        if (string.Equals(environment, "Development", StringComparison.OrdinalIgnoreCase)) {
+            MinimumLevel = developmentLevel;
+        } else if (string.Equals(environment, "Staging", StringComparison.OrdinalIgnoreCase)) {
+            MinimumLevel = stagingLevel;
+        }
+    }
+
+    public EnvironmentLoggingLevelSwitch(LogEventLevel developmentLevel = LogEventLevel.Debug,
+        LogEventLevel productionLevel = LogEventLevel.Information)
+        : this(developmentLevel, productionLevel, productionLevel) {
+        // no-op
     }
 }
