672b66cc57619e8ea0d45bb5664cc411a76340e5
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2025-07-24 01:19:08 +1200

    Add CollectionUtils and StringUtils

---
+++ LXGaming.Common/Utilities/CollectionUtils.cs
@@ -0,0 +1,26 @@
+﻿namespace LXGaming.Common.Utilities;
+
+public static class CollectionUtils {
+
+    public static void AddIgnoreNull<T>(ICollection<T> collection, T? value) {
+        if (value != null) {
+            collection.Add(value);
+        }
+    }
+
+    public static void AddIgnoreNull<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key, TValue? value) {
+        if (value != null) {
+            dictionary.Add(key, value);
+        }
+    }
+
+    public static bool TryAddIgnoreNull<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key, TValue? value) {
+        return value != null && dictionary.TryAdd(key, value);
+    }
+
+    public static void AddRange<T>(ICollection<T> collection, IEnumerable<T> items) {
+        foreach (var item in items) {
+            collection.Add(item);
+        }
+    }
+}
---
+++ LXGaming.Common/Utilities/StringUtils.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace LXGaming.Common.Utilities;
+
+public static class StringUtils {
+
+    public static int CountMatches(string @string, string searchString,
+        StringComparison stringComparison = StringComparison.Ordinal) {
+        if (string.IsNullOrEmpty(@string) || string.IsNullOrEmpty(searchString)) {
+            return 0;
+        }
+
+        var count = 0;
+        var index = 0;
+        while ((index = @string.IndexOf(searchString, index, stringComparison)) != -1) {
+            count++;
+            index += searchString.Length;
+        }
+
+        return count;
+    }
+
+    public static string GetEnumName(Enum @enum) {
+        var name = @enum.ToString();
+        var field = @enum.GetType().GetField(name, BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        var attribute = field?.GetCustomAttribute<EnumMemberAttribute>(false);
+        return attribute?.Value ?? name;
+    }
+
+    public static string Join(char separator, params object?[] values) {
+        return string.Join(separator, values
+            .Select(value => value?.ToString())
+            .Where(value => !string.IsNullOrEmpty(value)));
+    }
+
+    public static string ToHex(byte[] bytes) {
+        var stringBuilder = new StringBuilder(bytes.Length * 2);
+        foreach (var @byte in bytes) {
+            stringBuilder.Append(@byte.ToString("x2"));
+        }
+
+        return stringBuilder.ToString();
+    }
+}
