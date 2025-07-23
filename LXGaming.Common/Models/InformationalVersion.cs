0695008f70dea502991c804a12eced54ac6eb86b
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2025-07-24 01:17:00 +1200

    Refactor AssemblyUtils

---
+++ LXGaming.Common/Models/InformationalVersion.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+
+namespace LXGaming.Common.Models;
+
+public record InformationalVersion {
+
+    public required string Version { get; init; }
+
+    public string? PreRelease { get; init; }
+
+    public string? BuildMetadata { get; init; }
+
+    public static InformationalVersion Parse(string value) {
+        var minusIndex = value.IndexOf('-');
+        var plusIndex = value.LastIndexOf('+');
+
+        // Version
+        if (minusIndex == -1 && plusIndex == -1) {
+            return new InformationalVersion {
+                Version = value
+            };
+        }
+
+        // Version-PreRelease
+        if (minusIndex != -1 && plusIndex == -1) {
+            return new InformationalVersion {
+                Version = value[..minusIndex],
+                PreRelease = value[(minusIndex + 1)..]
+            };
+        }
+
+        // Version+BuildMetadata
+        if (minusIndex == -1 && plusIndex != -1) {
+            return new InformationalVersion {
+                Version = value[..plusIndex],
+                BuildMetadata = value[(plusIndex + 1)..]
+            };
+        }
+
+        // Version+BuildMetadata-PreRelease
+        if (minusIndex > plusIndex) {
+            // Everything after plus is BuildMetadata
+            return new InformationalVersion {
+                Version = value[..plusIndex],
+                BuildMetadata = value[(plusIndex + 1)..]
+            };
+        }
+
+        // Version-PreRelease+BuildMetadata
+        return new InformationalVersion {
+            Version = value[..minusIndex],
+            PreRelease = value.Substring(minusIndex + 1, plusIndex - minusIndex - 1),
+            BuildMetadata = value[(plusIndex + 1)..]
+        };
+    }
+
+    /// <inheritdoc />
+    public override string ToString() {
+        var stringBuilder = new StringBuilder(Version);
+        if (PreRelease != null) {
+            stringBuilder.Append('-').Append(PreRelease);
+        }
+
+        if (BuildMetadata != null) {
+            stringBuilder.Append('+').Append(BuildMetadata);
+        }
+
+        return stringBuilder.ToString();
+    }
+}
--- LXGaming.Common/Utilities/AssemblyUtils.cs
+++ LXGaming.Common/Utilities/AssemblyUtils.cs
@@ -1,34 +1,26 @@
 ﻿using System.Reflection;
+using LXGaming.Common.Models;
 
 namespace LXGaming.Common.Utilities;
 
 public static class AssemblyUtils {
 
-    public static string GetDescription(string assemblyString, string? name = null) {
-        return GetDescription(Assembly.Load(assemblyString), name);
+    public static string CreateDescription(string assemblyString, string? name = null) {
+        return CreateDescription(Assembly.Load(assemblyString), name);
     }
 
-    public static string GetDescription(Assembly assembly, string? name = null) {
-        return $"{name ?? GetName(assembly, "Unknown")} v{GetVersion(assembly, "Unknown")}";
+    public static string CreateDescription(Assembly assembly, string? name = null) {
+        return $"{name ?? assembly.GetName().Name ?? "Unknown"} v{GetVersion(assembly) ?? "Unknown"}";
     }
 
-    public static string GetName(Assembly assembly, string defaultValue) {
-        return GetName(assembly) ?? defaultValue;
-    }
-
-    public static string? GetName(Assembly assembly) {
-        return assembly.GetName().Name;
-    }
-
-    public static string GetVersion(Assembly assembly, string defaultValue) {
-        return GetVersion(assembly) ?? defaultValue;
+    public static InformationalVersion? GetInformationalVersion(Assembly assembly) {
+        var value = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+        return value != null ? InformationalVersion.Parse(value) : null;
     }
 
     public static string? GetVersion(Assembly assembly) {
-        return (
-            assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
-            ?? assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version
-            ?? assembly.GetName().Version?.ToString()
-        )?.Split('+', '-')[0];
+        return GetInformationalVersion(assembly)?.Version
+               ?? assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version
+               ?? assembly.GetName().Version?.ToString();
     }
 }
