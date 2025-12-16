ca0d2a446d732fdad964c6a5af26f817e0152433
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2025-12-16 23:41:05 +1300

    Add AsyncEventHandler Extensions

---
+++ LXGaming.Common/Event/Extensions.cs
@@ -0,0 +1,27 @@
+﻿namespace LXGaming.Common.Event;
+
+public static class Extensions {
+
+    public static Task InvokeAsync(this AsyncEventHandler? eventHandler, object? sender, EventArgs eventArgs) {
+        if (eventHandler == null) {
+            return Task.CompletedTask;
+        }
+
+        var tasks = eventHandler.GetInvocationList()
+            .Cast<AsyncEventHandler>()
+            .Select(handler => handler(sender, eventArgs));
+        return Task.WhenAll(tasks);
+    }
+
+    public static Task InvokeAsync<TEventArgs>(this AsyncEventHandler<TEventArgs>? eventHandler, object? sender,
+        TEventArgs eventArgs) {
+        if (eventHandler == null) {
+            return Task.CompletedTask;
+        }
+
+        var tasks = eventHandler.GetInvocationList()
+            .Cast<AsyncEventHandler<TEventArgs>>()
+            .Select(handler => handler(sender, eventArgs));
+        return Task.WhenAll(tasks);
+    }
+}
