4d880c55a45b91d88fec79a468b6ca16b9166ee1
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2025-12-16 23:39:37 +1300

    Add non-generic AsyncEventHandler

--- LXGaming.Common/Event/AsyncEventHandler.cs
+++ LXGaming.Common/Event/AsyncEventHandler.cs
@@ -1,3 +1,5 @@
 ﻿namespace LXGaming.Common.Event;
 
+public delegate Task AsyncEventHandler(object? sender, EventArgs eventArgs);
+
 public delegate Task AsyncEventHandler<in TEventArgs>(object? sender, TEventArgs eventArgs);
